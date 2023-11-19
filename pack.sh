d8064a507730a9bada0e65156c13fa388fc3a176
liulinghui <liulinghui@alibaba-inc.com>
2023-11-19 23:04:40 +0800

    Revert "insert new in front of old"
    This reverts commit 4c82235e37c9d538539b29b54880a349d5f5d5a6.

--- pack.sh
+++ pack.sh
@@ -1,1 +1,1 @@
-nice -n -10 reprozip trace  --overwrite  bash ./run.sh&& rm -f submission.rpz && time reprozip pack submission.rpz
+reprozip trace  --overwrite  bash ./run.sh&& rm -f submission.rpz && time reprozip pack submission.rpz
